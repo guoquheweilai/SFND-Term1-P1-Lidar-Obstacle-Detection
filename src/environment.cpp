927477ceebc56e4e7e6b336e5a4921cd9c8e98be
jinchaolu <30065376+jinchaolu@users.noreply.github.com>
2020-01-14 20:06:38 +0000

    feat: Create point processor
    Create point processor
    Add git ignore file

--- src/environment.cpp
+++ src/environment.cpp
@@ -48,6 +48,8 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // TODO:: Create lidar sensor 
     Lidar* lidar = new Lidar(cars, 0);
     // TODO:: Create point processor
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
+    renderRays(viewer, lidar->position, inputCloud);
   
 }
 
